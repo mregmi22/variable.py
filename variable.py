de953b61a6832a5918af4d97e97d2547191baaef
Manoj Regmi <manoj@Manojs-MacBook-Pro.local>
2023-03-15 05:20:02 -0600

    Variable with f-string

---
+++ variable.py
@@ -0,0 +1,12 @@
+
+# setting up the variable
+
+#print("Enter your age")
+#Age = input()
+# print("You are",  Age,  "years old")
+
+print("Enter your age")
+Age = input()
+# using f string
+print(f"You are {Age} years old")
+
