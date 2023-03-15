f171af7cf5617701c69a65c8c27e3a95ac2cf968
Manoj Regmi <manoj@Manojs-MacBook-Pro.local>
2023-03-15 06:24:23 -0600

    Simple shape calcutor

---
+++ Rectangle_calculator.py
@@ -0,0 +1,14 @@
+#getting the length/width/height from the user and setting it up to float
+length = float(input("Enter the length of a rectangle: "))
+width = float(input("Enter the width of a rectangle: "))
+height = float(input("Enter the height of a rectangle: "))
+
+#calculating the area and volume of rectangle 
+area = length * width 
+volume = length * width * height
+print(f"The area for this rectangle is: {area} ft")
+print(f"The volume for this rectangle is: {volume} ft^3")
+
+
+
+
